11446cbff8b6070f5c4d702c331a0cf6ec1d7946
BJ Firmacion <bj.firmacion@gmail.com>
2021-01-15 16:12:59 +0800

    retry netlify form 2

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { navigate } from "gatsby-link";
+
+function encode(data) {
+  const formData = new FormData();
+  for (const key of Object.keys(data)) {
+    formData.append(key, data[key]);
+  }
+  return formData;
+}
 
 const NewsLetter = () => {
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [attachment, setAttachment] = React.useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        name,
+        email,
+        attachment
+      })
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
+
   return (
     <Layout>
       <section className="newsletter-page">
@@ -15,12 +44,33 @@ const NewsLetter = () => {
             data-netlify="true"
             data-netlify-honeypot="bot-field"
             action="/success"
+            onSubmit={handleSubmit}
           >
             <input type="hidden" name="bot-field" /> {/* required for Netlify forms */}
             <input type="hidden" name="form-name" value="contact" /> {/* required for Netlify forms */}
-            <input type="text" name="name" placeholder="Your name" className="form-control" />
-            <input type="email" name="email" placeholder="Your email" className="form-control" />
-            <input type="file" name="file" placeholder="Attachment" className="form-control" />
+            <input
+              type="text"
+              name="name"
+              placeholder="Your name"
+              className="form-control"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your email"
+              className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="file"
+              name="attachment"
+              placeholder="Attachment"
+              className="form-control"
+              onChange={(e) => setAttachment(e.target.files[0])}
+            />
             <button type="submit" className="btn form-control submit-btn">subscribe</button>
           </form>
         </div>
